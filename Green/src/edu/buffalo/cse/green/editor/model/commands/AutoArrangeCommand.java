2328cb74337ec82f277ab0bc5fafcc556434447c
Luize Steina <luize0steina@gmail.com>
2019-07-19 16:10:51 +0300

    AutoLayout now draws trees individually

--- Green/src/edu/buffalo/cse/green/editor/model/commands/AutoArrangeCommand.java
+++ Green/src/edu/buffalo/cse/green/editor/model/commands/AutoArrangeCommand.java
@@ -14,6 +14,7 @@
 package edu.buffalo.cse.green.editor.model.commands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -22,6 +23,8 @@ import java.util.Vector;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.emf.common.util.Reflect;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 
 import ccvisu.GraphData;
 import ccvisu.GraphEdge;
@@ -169,7 +172,8 @@ public class AutoArrangeCommand extends Command {
 	    }
 	
 	// A more simple hierarchical model
-	public void execute() {
+	// Actually works
+	public void execute3() {
 	        DiagramEditor editor = DiagramEditor.getActiveEditor();
 	        RootModel root = editor.getRootModel();
 	        List<RelationshipModel> rels = root.getRelationships();	 
@@ -184,7 +188,8 @@ public class AutoArrangeCommand extends Command {
 	            if (m instanceof TypeModel) {
 	            	int lvl = getLevel((TypeModel)m);
 	            	int level = maxLevel - lvl;
-	            	System.out.println(m.getClass().getSimpleName() + " level: " + level);
+	            	System.out.println(m.getClass().getSimpleName() + " level: " + level
+	            			+ "Incoming: " + ((TypeModel) m).getIncomingEdges().size());
 	            	m.setSize(200, 100);
 	            	m.setLocation(posX[level], (level * (-150)));
 	            	posX[level] += 300;
@@ -194,6 +199,98 @@ public class AutoArrangeCommand extends Command {
 
 	        editor.checkDirty();
 	
+	}
+	
+	public void execute() {
+		DiagramEditor editor = DiagramEditor.getActiveEditor();
+        RootModel root = editor.getRootModel();
+		List<AbstractModel> children = root.getChildren();
+		System.out.println("Children: " + children);
+		int maxLevel = getMaxLevel(children);
+		List<TypeModel> tops = new ArrayList<>();
+		int nextX = 0;
+		List<String> classes = new ArrayList<>();
+		
+		
+		System.out.println("Execute");
+		
+		for(AbstractModel mod : children) {
+			if(mod instanceof TypeModel) {
+				try {
+					if((((TypeModel) mod).getType().getSuperclassName()) == null) {
+						tops.add((TypeModel) mod);
+					}
+				} catch (JavaModelException e) {
+				}
+			}
+		}
+		
+		System.out.println(tops);
+		
+		for(AbstractModel t : tops) {
+			if(t instanceof TypeModel) {
+				System.out.println("New tree");
+				nextX = drawTree8((TypeModel) t, maxLevel, nextX);
+			}
+		}
+		
+		editor.checkDirty();
+		
+	}
+	
+	
+	public int drawTree8(TypeModel top, int max, int prevX) {
+        DiagramEditor editor = DiagramEditor.getActiveEditor();
+        List<AbstractModel> allModels = getAllModels(top);
+        System.out.println("AllModels: " + allModels);
+        int[] xPos = new int[max + 1];
+        Arrays.fill(xPos, (prevX + 100));
+        int nextX = 0;
+           	    	        
+        System.out.println("MaxLevel: " + max);
+        
+        for (AbstractModel m : allModels) {
+            if (m instanceof TypeModel) {
+            	int lvl = getLevel((TypeModel)m);
+            	int level = max - lvl;
+            	System.out.println(m.getClass().getSimpleName() + " level: " + level
+            			+ "Incoming: " + ((TypeModel) m).getIncomingEdges().size());
+            	m.setSize(200, 100);
+            	m.setLocation(xPos[level], (level * (-150)));
+            	xPos[level] += 300;
+            }
+        }
+        
+        for(int i = 0; i < xPos.length; i++) {
+        	if(xPos[i] > nextX)
+        		nextX = xPos[i];
+        }
+ 
+
+        editor.checkDirty();
+        return nextX;
+
+}
+	
+	public List<AbstractModel> getAllModels(TypeModel top) {
+		List<AbstractModel> c = new ArrayList<>();
+		Set<RelationshipModel> set = top.getIncomingEdges();
+		System.out.println("Outgoing: " + set);
+		int repeat = set.size();
+		
+		if(repeat != 0) {
+			for (Iterator<RelationshipModel> it = set.iterator(); it.hasNext(); ) {
+			       RelationshipModel f = it.next();
+			       List<AbstractModel> temp = (getAllModels(f.getSourceModel()));
+			       	for(AbstractModel m : temp) {
+			       		c.add(m);
+			       	}
+			   }
+			 
+		}
+		else {}
+		c.add(top);
+		return c;
 	}
 	
 	// Finds the maximum depth out of a list of TypeModels
@@ -233,6 +330,8 @@ public class AutoArrangeCommand extends Command {
 			}
 		return maxLevel;
 	}
+	
+	
 	
 	
 }
