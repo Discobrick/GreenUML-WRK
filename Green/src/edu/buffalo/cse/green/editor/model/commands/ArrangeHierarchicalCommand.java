7734b4bcd8c446e83c7ec522f3cbdba058517a18
Emils Kalderauskis <emils.kalderauskis@gmail.com>
2019-07-22 08:51:13 +0300

    Added keyboard shortcuts to arrange commands

--- Green/src/edu/buffalo/cse/green/editor/model/commands/ArrangeHierarchicalCommand.java
+++ Green/src/edu/buffalo/cse/green/editor/model/commands/ArrangeHierarchicalCommand.java
@@ -14,6 +14,7 @@
 package edu.buffalo.cse.green.editor.model.commands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -50,91 +51,151 @@ public class ArrangeHierarchicalCommand extends Command {
 	private int[][] opos;
 	private int[][] npos;
 	private Vector<TypeModel> _m;
-	
+
 	/**
 	 * 
 	 */
 	public ArrangeHierarchicalCommand() {
 		_m = new Vector<TypeModel>();
 	}
-	
+
 	public void undo() {
-		for( int i=0; i<_m.size(); i++)
+		for (int i = 0; i < _m.size(); i++)
 			_m.get(i).setLocation(opos[i][0], opos[i][1]);
 	}
-	
+
 	public void redo() {
-		for( int i=0; i<_m.size(); i++)
+		for (int i = 0; i < _m.size(); i++)
 			_m.get(i).setLocation(npos[i][0], npos[i][1]);
 	}
-		
+
 	// A more simple hierarchical model
 	public void execute() {
-	        DiagramEditor editor = DiagramEditor.getActiveEditor();
-	        RootModel root = editor.getRootModel();
-	        List<RelationshipModel> rels = root.getRelationships();	 
-	        List<AbstractModel> mods = root.getChildren();
-	        int maxLevel = getMaxLevel(mods);
-	        int[] posX = new int[maxLevel+1];
-	        
-	       	    	        
-	        System.out.println("MaxLevel: " + maxLevel);
-	        
-	        for (AbstractModel m : mods) {
-	            if (m instanceof TypeModel) {
-	            	int lvl = getLevel((TypeModel)m);
-	            	int level = maxLevel - lvl;
-	            	System.out.println(m.getClass().getSimpleName() + " level: " + level);
-	            	m.setSize(200, 100);
-	            	m.setLocation(posX[level], (level * (-150)));
-	            	posX[level] += 300;
-	            }
-	        }
-	 
+		DiagramEditor editor = DiagramEditor.getActiveEditor();
+		RootModel root = editor.getRootModel();
+		List<AbstractModel> children = root.getChildren();
+		System.out.println("Children: " + children);
+		int maxLevel = getMaxLevel(children);
+		List<TypeModel> tops = new ArrayList<>();
+		int nextX = 0;
+		List<String> classes = new ArrayList<>();
 
-	        editor.checkDirty();
-	
+		System.out.println("Execute");
+
+		for (AbstractModel mod : children) {
+			if (mod instanceof TypeModel) {
+				try {
+					if ((((TypeModel) mod).getType().getSuperclassName()) == null) {
+						tops.add((TypeModel) mod);
+					}
+				} catch (JavaModelException e) {
+				}
+			}
+		}
+
+		System.out.println(tops);
+
+		for (AbstractModel t : tops) {
+			if (t instanceof TypeModel) {
+				System.out.println("New tree");
+				nextX = drawTree8((TypeModel) t, maxLevel, nextX);
+			}
+		}
+
+		editor.checkDirty();
+		
+		
 	}
-	
+
+	public int drawTree8(TypeModel top, int max, int prevX) {
+		DiagramEditor editor = DiagramEditor.getActiveEditor();
+		List<AbstractModel> allModels = getAllModels(top);
+		//System.out.println("AllModels: " + allModels);
+		int[] xPos = new int[max + 1];
+		Arrays.fill(xPos, (prevX + 100));
+		int nextX = 0;
+
+		System.out.println("MaxLevel: " + max);
+
+		for (AbstractModel m : allModels) {
+			if (m instanceof TypeModel) {
+				int lvl = getLevel((TypeModel) m);
+				int level = max - lvl;
+				System.out.println(m.getClass().getSimpleName() + " level: " + level + "Incoming: "
+						+ ((TypeModel) m).getIncomingEdges().size());
+				m.setSize(200, 100);
+				m.setLocation(xPos[level], (level * (-150)));
+				xPos[level] += 100;
+			}
+		}
+
+		for (int i = 0; i < xPos.length; i++) {
+			if (xPos[i] > nextX)
+				nextX = xPos[i];
+		}
+		
+		editor.checkDirty();
+		return nextX;
+
+
+	}
+
+	public List<AbstractModel> getAllModels(TypeModel top) {
+		List<AbstractModel> c = new ArrayList<>();
+		Set<RelationshipModel> set = top.getIncomingEdges();
+		System.out.println("Outgoing: " + set);
+		int repeat = set.size();
+
+		if (repeat != 0) {
+			for (Iterator<RelationshipModel> it = set.iterator(); it.hasNext();) {
+				RelationshipModel f = it.next();
+				List<AbstractModel> temp = (getAllModels(f.getSourceModel()));
+				for (AbstractModel m : temp) {
+					c.add(m);
+				}
+			}
+
+		} else {
+		}
+		c.add(top);
+		return c;
+	}
+
 	// Finds the maximum depth out of a list of TypeModels
 	public int getMaxLevel(List<AbstractModel> list) {
 		int maxLevel = 0;
-		
-		for(AbstractModel mod : list) {
-			if(mod instanceof TypeModel) {
 
-
+		for (AbstractModel mod : list) {
+			if (mod instanceof TypeModel) {
 				int lvl = getLevel((TypeModel) mod);
-				if(lvl > maxLevel) {
+				if (lvl > maxLevel) {
 					maxLevel = lvl;
 				}
 			}
 		}
-		
+
 		return maxLevel;
 	}
-	
+
 	// Finds the depth for a single TypeModel
 	public int getLevel(TypeModel mod) {
-		
+
 		int maxLevel = 0;
 		Set<RelationshipModel> set = mod.getOutgoingEdges();
 		int repeat = set.size();
-		
-		if(repeat != 0) {
-			for (Iterator<RelationshipModel> it = set.iterator(); it.hasNext(); ) {
-			       RelationshipModel f = it.next();
-			       	int lvl = getLevel(f.getTargetModel()) +1;
-			       	if(lvl > maxLevel) {
-			       		maxLevel = lvl;
-			       	}
-			   }
-		}
-		else {
-			return 0;
+
+		if (repeat != 0) {
+			for (Iterator<RelationshipModel> it = set.iterator(); it.hasNext();) {
+				RelationshipModel f = it.next();
+				int lvl = getLevel(f.getTargetModel()) + 1;
+				if (lvl > maxLevel) {
+					maxLevel = lvl;
+				}
 			}
+		} else {
+			return 0;
+		}
 		return maxLevel;
 	}
-	
-	
+
 }
