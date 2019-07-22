412b6e42de7a4c5edfde43a709ab485cc97a529a
Luize Steina <luize0steina@gmail.com>
2019-07-22 10:32:31 +0300

    minor changes

--- Green/src/edu/buffalo/cse/green/editor/model/commands/AutoArrangeCommand.java
+++ Green/src/edu/buffalo/cse/green/editor/model/commands/AutoArrangeCommand.java
@@ -71,148 +71,18 @@ public class AutoArrangeCommand extends Command {
 	}
 	
 	
-	// Optimized layout for more complicated graphs
-	public void execute2() {
-	        DiagramEditor editor = DiagramEditor.getActiveEditor();
-	        RootModel root = editor.getRootModel();
-	        List<RelationshipModel> rels = root.getRelationships();
-	 
-	        List<AbstractModel> mods = root.getChildren();
-
-	        GraphData gd = new GraphData();
-	        for (AbstractModel m : mods) {
-
-	            if (m instanceof TypeModel) {
-	 
-	                final TypeModel n = (TypeModel) m;
-	 
-	                GraphVertex me = new GraphVertex(n);
-	                me.id = gd.vertices.size();
-	                me.name = "" + me.id;
-	                Dimension size = editor.getRootPart().getPartFromModel(n).getFigure().getSize();
-	                me.degree = n.getIncomingEdges().size() + n.getOutgoingEdges().size()
-	                        + (size.height * size.width / 20000.0f);
-	                me.isSource = me.degree > 0;
-	                me.pos.x = n.getLocation().x / 200.0f;
-	                me.pos.y = n.getLocation().y / 200.0f;
-	                if (!gd.vertices.contains(me)) {
-	                    gd.vertices.add(me);
-	                    _m.add(n);
-	                } else
-	                    me = (GraphVertex) gd.vertices.get(gd.vertices.indexOf(me));
-
-	                for (RelationshipModel e : n.getOutgoingEdges()) {                   
-	                   
-	                    TypeModel y = e.getTargetModel();
-	                   
-	                    if (n.equals(y))
-	                        continue; // no reflexive graph
-	                    GraphVertex you = new GraphVertex(y);
-	                    you.id = gd.vertices.size();
-	                    you.name = "" + you.id;
-	                    you.degree = y.getIncomingEdges().size()
-	                            + y.getOutgoingEdges().size() /* (y.getSize().height + y.getSize().width) */;
-	                    you.isSource = you.degree > 0;
-	                    you.pos.x = y.getLocation().x / 200.0f;
-	                    you.pos.y = y.getLocation().y / 200.0f;
-	                    if (!gd.vertices.contains(you)) {
-	                        gd.vertices.add(you);
-	                        _m.add(y);
-	                    } else
-	                        you = (GraphVertex) gd.vertices.get(gd.vertices.indexOf(you));
-	 
-	                    // have me, you
-	                    // create edge
-	 
-	                    if (me.id != you.id) {
-	                        GraphEdge ed = new GraphEdge();
-	                        ed.x = me.id;
-	                        ed.y = you.id;
-	                        ed.w = 1.0f;
-	                        gd.edges.add(ed);
-	                    }
-	                    
-	                }
-	            }
-	        }
-	 
-	        Options options = CCVisuUtil.newOptions(gd, 100, 3, 1, false, false, 2.001f, null, false);
-	        Minimizer me = new MinimizerBarnesHut(options);
-
-	 
-	        me.minimizeEnergy();
-	 
-	        // normalize
-	        float lx = 999, ly = 999;
-	        for (ccvisu.GraphVertex v : gd.vertices) {
-	            if (v.pos.x < lx)
-	                lx = v.pos.x;
-	            if (v.pos.y < ly)
-	                ly = v.pos.y;
-	        }
-	 
-	        for (ccvisu.GraphVertex v : gd.vertices) {
-	            v.pos.x -= lx;
-	            v.pos.y -= ly;
-	        }
-	 
-	        opos = new int[_m.size()][2];
-	        npos = new int[_m.size()][2];
-	 
-	        for (int i = 0; i < gd.vertices.size(); i++) {
-	            opos[i][0] = _m.get(i).getLocation().x;
-	            opos[i][1] = _m.get(i).getLocation().y;
-	            GraphVertex v = (GraphVertex) gd.vertices.get(i);
-	            v.me.setLocation((int) (v.pos.x * 200), (int) (v.pos.y * 200));
-	            npos[i][0] = v.me.getLocation().x;
-	            npos[i][1] = v.me.getLocation().y;
-	        }
-
-	        editor.checkDirty();
-	    }
-	
-	// A more simple hierarchical model
-	// Actually works
-	public void execute3() {
-	        DiagramEditor editor = DiagramEditor.getActiveEditor();
-	        RootModel root = editor.getRootModel();
-	        List<RelationshipModel> rels = root.getRelationships();	 
-	        List<AbstractModel> mods = root.getChildren();
-	        int maxLevel = getMaxLevel(mods);
-	        int[] posX = new int[maxLevel+1];
-	        
-	       	    	        
-	        System.out.println("MaxLevel: " + maxLevel);
-	        
-	        for (AbstractModel m : mods) {
-	            if (m instanceof TypeModel) {
-	            	int lvl = getLevel((TypeModel)m);
-	            	int level = maxLevel - lvl;
-	            	System.out.println(m.getClass().getSimpleName() + " level: " + level
-	            			+ "Incoming: " + ((TypeModel) m).getIncomingEdges().size());
-	            	m.setSize(200, 100);
-	            	m.setLocation(posX[level], (level * (-150)));
-	            	posX[level] += 300;
-	            }
-	        }
-	 
-
-	        editor.checkDirty();
-	
-	}
-	
+	/* This method creates a diagram suited for tree-like structures 
+	 * by drawing trees one by one.
+	 */
 	public void execute() {
 		DiagramEditor editor = DiagramEditor.getActiveEditor();
         RootModel root = editor.getRootModel();
 		List<AbstractModel> children = root.getChildren();
-		System.out.println("Children: " + children);
 		int maxLevel = getMaxLevel(children);
+		// tops contains the classes that do not extend any other class shown in the diagram
 		List<TypeModel> tops = new ArrayList<>();
+		// nextX provides spacing between trees on the x axis
 		int nextX = 0;
-		List<String> classes = new ArrayList<>();
-		
-		
-		System.out.println("Execute");
 		
 		for(AbstractModel mod : children) {
 			if(mod instanceof TypeModel) {
@@ -225,11 +95,8 @@ public class AutoArrangeCommand extends Command {
 			}
 		}
 		
-		System.out.println(tops);
-		
 		for(AbstractModel t : tops) {
 			if(t instanceof TypeModel) {
-				System.out.println("New tree");
 				nextX = drawTree8((TypeModel) t, maxLevel, nextX);
 			}
 		}
@@ -238,23 +105,25 @@ public class AutoArrangeCommand extends Command {
 		
 	}
 	
-	
+	// Draws a tree for an individual superclass
 	public int drawTree8(TypeModel top, int max, int prevX) {
         DiagramEditor editor = DiagramEditor.getActiveEditor();
         List<AbstractModel> allModels = getAllModels(top);
         System.out.println("AllModels: " + allModels);
+        // xPos provides spacing on the x axis between the graphical elements
         int[] xPos = new int[max + 1];
         Arrays.fill(xPos, (prevX + 100));
         int nextX = 0;
-           	    	        
-        System.out.println("MaxLevel: " + max);
+        
+        /* Y coordinates for the objects are determined by their levels.
+         * These levels make the tree structure more comprehensible, as
+         * classes are placed under their parent class.     
+         */
         
         for (AbstractModel m : allModels) {
             if (m instanceof TypeModel) {
             	int lvl = getLevel((TypeModel)m);
             	int level = max - lvl;
-            	System.out.println(m.getClass().getSimpleName() + " level: " + level
-            			+ "Incoming: " + ((TypeModel) m).getIncomingEdges().size());
             	m.setSize(200, 100);
             	m.setLocation(xPos[level], (level * (-150)));
             	xPos[level] += 300;
@@ -265,13 +134,10 @@ public class AutoArrangeCommand extends Command {
         	if(xPos[i] > nextX)
         		nextX = xPos[i];
         }
- 
-
-        editor.checkDirty();
         return nextX;
-
 }
 	
+	// returns the given superclass model and all of its subclass models
 	public List<AbstractModel> getAllModels(TypeModel top) {
 		List<AbstractModel> c = new ArrayList<>();
 		Set<RelationshipModel> set = top.getIncomingEdges();
@@ -288,12 +154,11 @@ public class AutoArrangeCommand extends Command {
 			   }
 			 
 		}
-		else {}
 		c.add(top);
 		return c;
 	}
 	
-	// Finds the maximum depth out of a list of TypeModels
+	// Finds the maximum level(depth) out of a list of TypeModels
 	public int getMaxLevel(List<AbstractModel> list) {
 		int maxLevel = 0;
 		
@@ -309,7 +174,7 @@ public class AutoArrangeCommand extends Command {
 		return maxLevel;
 	}
 	
-	// Finds the depth for a single TypeModel
+	// Finds the level(depth) for a single TypeModel
 	public int getLevel(TypeModel mod) {
 		
 		int maxLevel = 0;
